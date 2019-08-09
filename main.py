a09145dee92cc6502c055c11f63286b12a55e87a
SpaskeS <djordje.spasojevic1996@gmail.com>
2019-08-09 19:02:53 +0200

    Dodate sve komande za gui

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter.messagebox
 
 # TODO: Srediti elemente po klasama
 # TODO: Izbaciti globalne promenljive ako je moguce
+# TODO: staviti da ne mogu da se biraju dva pocetka i dva kraja
+
 
 canvas_mode = None
 grana_pocetak = 0
@@ -26,44 +28,51 @@ def start_alg_3():
 
 def pocetak():
     print('Odredi pocetni cvor')
+    global canvas_mode
+    canvas_mode = 'pocetak'
 
 def kraj():
     print('Odredi krajnji cvor')
+    global canvas_mode
+    canvas_mode='kraj'
 
 def dodaj_cvor():
-    print('Dodajte cvor')
     global canvas_mode
     canvas_mode = 'dodaj_cvor'
     print('canvas_mode')
 
 def dodaj_granu():
-    print('dodajte granu')
     global canvas_mode
     canvas_mode = 'dodaj_granu'
 
-# TODO: napraviti funkcije za brisanje grana i cvorova
-
 def izbrisi_cvor():
-    print('brisi cvor')
     global canvas_mode
     canvas_mode = 'izbrisi_cvor'
 
 def izbrisi_granu():
-    print('brisi granu')
     global canvas_mode
     canvas_mode = 'izbrisi_granu'
+
+def dodaj_prepreku():
+    global canvas_mode
+    canvas_mode = 'dodaj_prepreku'
 
 
 def reset():
     if tkinter.messagebox.askquestion('Reset', 'Da li hocete da restartuje graf?') == 'yes':
         print('Restartujem graf')
-        global canvas, grane, cvorovi
+        global canvas, grane, cvorovi, canvas_mode
         canvas.delete(ALL)
         grane = {}
         cvorovi= {}
+        canvas_mode = None
 
-def quit():
-    exit(1)
+
+def grana_klik(event):
+    if canvas_mode == 'izbrisi_granu':
+        global grane
+        canvas.delete(event)
+        grane.pop(event)
 
 def canvas_klik(event):
     print(event.x)
@@ -72,13 +81,15 @@ def canvas_klik(event):
     x = event.x
     y = event.y
 
+    global cvorovi, grane
+
     if canvas_mode == 'dodaj_cvor':
         global cvorovi
         for (id, koordinate) in cvorovi.items():
             if abs(x-koordinate[0]) < 50 and abs(y-koordinate[1]) < 50:
                 print('Drugi cvor je previse blizu')
                 return
-        cvor = canvas.create_oval(x-15, y-15, x+15, y+15, outline='red', fill='black')
+        cvor = canvas.create_oval(x-15, y-15, x+15, y+15, outline='red', fill='gray')
         cvorovi[cvor] = (x, y)
 
     if canvas_mode == 'dodaj_granu':
@@ -96,10 +107,45 @@ def canvas_klik(event):
                         print('Cvor ne moze imati povratnih grana')
                         return
                     grana_pocetak=0
-                    grana = canvas.create_line(cvorovi[prvi_cvor][0], cvorovi[prvi_cvor][1], koordinate[0], koordinate[1], width=2, fill='blue')
+                    grana = canvas.create_line(cvorovi[prvi_cvor][0], cvorovi[prvi_cvor][1], koordinate[0], koordinate[1], width=4, fill='blue')
                     grane[grana] = (prvi_cvor, id)
                     canvas.tag_lower(grana)
+                    print(grana)
+                    canvas.tag_bind(grana, '<Button-1>', lambda g: grana_klik(grana))
                     prvi_cvor = None
+
+    if canvas_mode == 'izbrisi_cvor':
+
+        for (id, koordinate) in cvorovi.items():
+            if abs(koordinate[0]-x) < 15 and abs(koordinate[1]-y) < 15:
+                canvas.delete(id)
+                cvorovi.pop(id)
+                grane_za_brisanje = []
+                for(gid, cvorovi_grane) in grane.items():
+                    if id == cvorovi_grane[0] or id == cvorovi_grane[1]:
+                        canvas.delete(gid)
+                        grane_za_brisanje.append(gid)
+                for gid in grane_za_brisanje:
+                    grane.pop(gid)
+                return
+
+    if canvas_mode == 'pocetak':
+        for (id, koordinate) in cvorovi.items():
+            if abs(x-koordinate[0]) < 15 and abs(y-koordinate[1]) < 15:
+                canvas.itemconfig(id, fill='green')
+
+    if canvas_mode == 'kraj':
+        for (id, koordinate) in cvorovi.items():
+            if abs(koordinate[0]-x) < 15 and abs(koordinate[1]-y) < 15:
+                canvas.itemconfig(id, fill='red')
+
+    if canvas_mode == 'dodaj_prepreku':
+        for (id, koordinate) in cvorovi.items():
+            if abs(koordinate[0]-x) < 15 and abs(koordinate[1]-y) < 15:
+                canvas.itemconfig(id, fill='black')
+
+def quit():
+    exit(1)
 
 def main():
     root = Tk()
@@ -114,8 +160,9 @@ def main():
     button_dodaj_granu = Button(root, text='Dodaj granu', command=dodaj_granu, width=30)
     button_izbrisi_cvor = Button(root, text='Izbriši čvor', command=izbrisi_cvor, width=30)
     button_izbrisi_granu = Button(root, text='Izbriši granu', command=izbrisi_granu, width=30)
-    button_reset = Button(root, text='Reset', command=reset, width=64)
-    button_exit = Button(root, text='Exit', command= quit)
+    button_reset = Button(root, text='Reset', command=reset, width=30)
+    button_exit = Button(root, text='Exit', command= quit, width=30)
+    button_dodaj_prepreku = Button(root, text='Dodaj prepreku', command = dodaj_prepreku, width=30)
     global canvas
     canvas = Canvas(root, width = 800, height = 700, bg='white')
 
@@ -131,7 +178,8 @@ def main():
     button_izbrisi_cvor.grid(column=3, row=3)
     button_izbrisi_granu.grid(column=4, row=3)
     button_exit.grid(column=4, row=0)
-    button_reset.grid(column = 3, row=4, columnspan=2)
+    button_reset.grid(column = 4, row=4)
+    button_dodaj_prepreku.grid(column = 3, row=4)
     canvas.grid(row=1, column=0, columnspan=3, rowspan=4)
 
     root.grid_columnconfigure(0,weight=1)
