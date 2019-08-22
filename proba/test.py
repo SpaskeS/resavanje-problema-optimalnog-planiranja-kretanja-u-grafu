e3e02315e07840db9c8457400156b183d28a3586
Ana Petrovic <pana.petrovic@gmail.com>
2019-08-22 13:53:45 +0200

    match_n

--- proba/test.py
+++ proba/test.py
@@ -202,6 +202,17 @@ def tree_from_edge(g, node1, node2):
                     result.append(neighbor)
     return result
 
+def match_n(g,source,destination,max_n):
+    bi_graph = nx.Graph()
+    for dest_node in destination:
+        if dest_node != source and (dest_node.is_hole() or dest_node.robot) :
+            shortest = nx.shortest_path(g, source, dest_node)
+            for i in range(max_n):
+                c = Cvor('t',True,False)
+                bi_graph.add_edge(c, dest_node, weight = 1000-len(shortest), shortest_path = shortest)
+
+    return nx.max_weight_matching(bi_graph), bi_graph
+
 
 nodes = {'a','s','x','y',
             'b','c','e','f',
@@ -225,10 +236,9 @@ edges = [ ('a','s'),
           ('z', 'k')
  ]
 
-obstacles = ['b','c','e','f','g','t']
+obstacles = ['b','c','e','f','g','t', 'h']
 
 g = nx.Graph()
-
 
 for node in nodes:
     c = Cvor(node, node in obstacles, node == 's')
@@ -278,13 +288,42 @@ def animate(i):
     nx.draw_networkx(g, pos=pos,  with_labels=True, node_color=set_colors(), font_weight='bold'   )
 
 fig, ax = plt.subplots(figsize=(6,4))
-#ani = animation.FuncAnimation(fig, animate, frames=len(moves), init_func=init,
-#                              interval=400, repeat=False)
+ani = animation.FuncAnimation(fig, animate, frames=len(moves), init_func=init,
+                              interval=400, repeat=False)
+
+
+def topt_1(g, path):
+
+    plans = []
+    len_path = len(path)
+
+    right = tree_from_edge(g,path[-1], path[-2])
+    match, topt_graph = match_n(g, path[-1], right, len_path)
+
+    # prvi slucaj
+    plans.append(([], 0, 0, 0))
+
+    print(match)
+    for i in range(len_path):
+        moves = []
+        for m in match:
+            weight = 999 - topt_graph.edges[m[0], m[1]]['weight']
+            print(weight)
+            if weight <= i:
+                shortest = topt_graph.edges[m[0], m[1]]['shortest_path']
+                shortest = shortest.reverse()
+                print(shortest)
+                for i in len(shortest):
+                    if path[i].obstacle:
+                        moves.append((path[i], path[i-1]))
+
+        plans.append((moves, i, 0, 0))
+
+    return plans
 
 for path in nx.all_simple_paths(g,get_node_by_name(g, 's'),get_node_by_name(g,'t'),10000):
 #    print(topt(g, path, 1))
 
-    d = topt(g, list(path))
 #    print('topt:')
 #    for k in d:
 #        print(str(k) + path_str(d[k]))
@@ -320,13 +359,15 @@ for path in nx.all_simple_paths(g,get_node_by_name(g, 's'),get_node_by_name(g,'t
     right = tree_from_edge(g, get_node_by_name(g, 'b'), get_node_by_name(g, 's'))
 
     bi = bipartitive_graph(g, right, left)
-    nx.draw_networkx(bi, with_labels=True, node_color=set_colors(), font_weight='bold')
+#    nx.draw_networkx(bi, with_labels=True, font_weight='bold')
     match = nx.max_weight_matching(bi)
     for m in match:
-        weight = bi.edges[m[0], m[1]]['weight']
+        weight = bi.edges[m[0], m[1]]['weight']-1
         shortest_path_edge_bi = bi.edges[m[0], m[1]]['shortest_path']
         print(1000 - weight)
         print(path_str(shortest_path_edge_bi))
-        print(m[0].name + '_' + m[1].name)
+        print(m[0].name + '---->' + m[1].name)
+
+    print(topt_1(g, path))
 
 plt.show()
